afca3ab5cb021bc5090918872c96d97146e44642
unknown <fer.dog9714@gmail.com>
2020-05-31 05:08:15 -0500

    Holi, ayudame

--- BD/abarrotes.sql
+++ BD/abarrotes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3307
--- Generation Time: May 19, 2020 at 04:52 AM
+-- Host: localhost:3306
+-- Generation Time: May 31, 2020 at 10:05 AM
 -- Server version: 5.7.24
--- PHP Version: 7.3.7
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,6 +35,18 @@ CREATE TABLE `inventario` (
   `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `inventario`
+--
+
+INSERT INTO `inventario` (`id_inventario`, `descripcion`, `precio`, `cantidad`) VALUES
+(1, 'huevos', 15, 18),
+(2, 'Jabon-zote', 20, 200),
+(3, 'Galletas', 12.5, 20),
+(4, 'Rollo-papel', 2.5, 30),
+(5, 'Servilletas', 15, 15),
+(6, 'Tortillas', 15, 9);
+
 -- --------------------------------------------------------
 
 --
@@ -47,8 +59,19 @@ CREATE TABLE `inventario_venta` (
   `id_inventario` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `cantidad_venta` int(11) NOT NULL,
-  `total` int(11) NOT NULL
+  `total` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `inventario_venta`
+--
+
+INSERT INTO `inventario_venta` (`id_inventario_venta`, `id_venta`, `id_inventario`, `cantidad`, `cantidad_venta`, `total`) VALUES
+(1, 1, 1, 18, 5, 75),
+(2, 1, 3, 20, 5, 62.5),
+(3, 1, 4, 30, 8, 20),
+(5, 1, 2, 200, 3, 60),
+(6, 5, 6, 9, 2, 30);
 
 -- --------------------------------------------------------
 
@@ -58,8 +81,17 @@ CREATE TABLE `inventario_venta` (
 
 CREATE TABLE `venta` (
   `id_venta` int(11) NOT NULL,
-  `total` float NOT NULL
+  `total` float NOT NULL,
+  `finalizado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `venta`
+--
+
+INSERT INTO `venta` (`id_venta`, `total`, `finalizado`) VALUES
+(1, 217.5, 1),
+(5, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -93,19 +125,19 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT for table `inventario`
 --
 ALTER TABLE `inventario`
-  MODIFY `id_inventario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_inventario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `inventario_venta`
 --
 ALTER TABLE `inventario_venta`
-  MODIFY `id_inventario_venta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_inventario_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -115,8 +147,8 @@ ALTER TABLE `venta`
 -- Constraints for table `inventario_venta`
 --
 ALTER TABLE `inventario_venta`
-  ADD CONSTRAINT `fk_venta_inventario_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_venta_inventario_2` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id_inventario`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `id_inventario` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id_inventario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_venta` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
